70701fd1d8ee6ef5a9c50c91afed394e7a38f775
BoreyJr <52574681+BoreyJr@users.noreply.github.com>
2020-04-15 16:50:05 +0700

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,9 +1,30 @@
 #include<stdio.h>
+int spouse,inpendent;
+
+void intro();
+void emp_crc();
+void mainfunction();
+
 int main()
 {
-  
-
+  intro();
+  emp_scrc();
+  mainfunction();
+	
 
 return 0;
 }
+void intro() //RakSmey
+{
+printf("Cambodia Salary tax");
+}
+void emp_crc() //Bunrithsothy
+{
+	
+}
+void mainfunction () //1.borey //2.Pich & Hong //3.Mr.Hao
+{
+
+}
+
 
